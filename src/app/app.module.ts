7be109ff6a82e99c1230d9a426957e9a2b9f385c
alvaro.rodrigues <alvaro.rodrigues@mvrec.local>
2022-03-01 14:04:18 -0300

    Implementando o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { HomeComponent } from './home/home.component';
+import { TopoComponent } from './topo/topo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodapeComponent,
+    HomeComponent,
+    TopoComponent
   ],
   imports: [
     BrowserModule
