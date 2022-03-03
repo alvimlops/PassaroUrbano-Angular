fbb0a72813436e0f65a8f1f012f470466f0a571f
alvaro.rodrigues <alvaro.rodrigues@mvrec.local>
2022-03-03 06:29:59 -0300

    Para validação

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
+import { Oferta } from './../shared/oferta.model';
 import { Component, OnInit } from '@angular/core';
+import { provideRoutes } from '@angular/router';
+import { OfertaService } from '../oferta.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertaService ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Oferta[] | undefined
+
+  constructor(private ofertaService: OfertaService) { }
 
   ngOnInit(): void {
+    this.ofertas = this.ofertaService.getOfertas()
   }
 
 }
---
+++ src/app/shared/oferta.model.ts
@@ -0,0 +1,12 @@
+export class Oferta {
+  public id: number | undefined
+  public categoria: string | undefined
+  public titulo: string | undefined
+  public descricao_oferta: string | undefined
+  public anunciante: string | undefined
+  public valor: number | undefined
+  public destaque: boolean | undefined
+  public imagens: Array<object>[] | undefined
+
+
+}
