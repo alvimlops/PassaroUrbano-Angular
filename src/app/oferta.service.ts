4da88b895e4ebb0ce2ef953a94cd50b6f56c6ae9
alvaro.rodrigues <alvaro.rodrigues@mvrec.local>
2022-03-03 05:53:45 -0300

    Injeção de dependencia

--- src/app/oferta.service.ts
+++ src/app/oferta.service.ts
@@ -1,6 +1,6 @@
 export class OfertaService {
-  public getOfertas(): Array<string>{
+  public getOfertas(): Array<string> {
     let ofertas = ['oferta1', 'oferta2', 'oferta3',]
     return ofertas;
-}
+  }
 }
